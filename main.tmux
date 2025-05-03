33cdc382edb6bd0d4e39982d435ed41be7b1180e
Svetlin Ralchev <iamralch@users.noreply.github.com>
2025-05-03 11:05:37 +0400

    feat(tmux): refactor theme management for better modularity
    Refactored the tmux theme management scripts to improve modularity and
    maintainability. Key changes include:
    
    - Replaced `dark-mode.sh` and `dark-mode.swift` with a unified
      `tmux-theme.sh` script.
    - Renamed `helpers.sh` to `tmux-core.sh` for clarity.
    - Updated `main.tmux` to use the new `tmux-theme.sh` script.
    - Removed redundant files and streamlined theme selection logic.
    
    These changes simplify the codebase and enhance theme configuration.
    
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

--- main.tmux
+++ main.tmux
@@ -3,13 +3,13 @@
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=1090
-source "${CURRENT_DIR}/scripts/helpers.sh"
-
-# shellcheck disable=1090
-THEME_PATH="${CURRENT_DIR}/scripts/dark-mode.sh"
+source "${CURRENT_DIR}/scripts/tmux-core.sh"
 
 main() {
-	"${CURRENT_DIR}/scripts/dark-mode.swift" "${THEME_PATH}" &
+	# shellcheck disable=1090
+	opt_theme="$("${CURRENT_DIR}"/scripts/tmux-theme.sh)"
+
+	tmux_source_file "$opt_theme"
 }
 
 main
--- scripts/tmux-theme.sh
+++ scripts/tmux-theme.sh
@@ -3,10 +3,9 @@
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=1090
-source "${CURRENT_DIR}/helpers.sh"
+source "${CURRENT_DIR}/tmux-core.sh"
 
 main() {
-	local opt_theme
 	local opt_theme_dark
 	local opt_theme_light
 
@@ -14,14 +13,15 @@ main() {
 	tmux set -g @theme_light "$HOME/.config/tmux/themes/catppuccin-latte.tmux"
 
 	# dark & light themes
-	opt_theme_dark="$(get_tmux_option "@theme_dark")"
-	opt_theme_light="$(get_tmux_option "@theme_light")"
+	opt_theme_light="$(tmux_get_option "@theme_light")"
+	opt_theme_dark="$(tmux_get_option "@theme_dark")"
+	opt_theme_dark_mode="$("${CURRENT_DIR}/tmux-theme-dark.scpt")"
 
-	opt_theme="$1"
-	opt_theme="${opt_theme/dark/$opt_theme_dark}"
-	opt_theme="${opt_theme/light/$opt_theme_light}"
-
-	source_tmux_file "$opt_theme"
+	if [[ "$opt_theme_dark_mode" == "true" ]]; then
+		echo "$opt_theme_dark"
+	else
+		echo "$opt_theme_light"
+	fi
 }
 
 main "$@"
